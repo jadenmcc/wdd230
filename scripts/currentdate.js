4675002deb33251f9ead971b6cf6133b6899c9a2
jadenmcc <jadenmccarrey@gmail.coms>
2022-09-16 09:40:21 -0600

    Added script for dynamic date.

---
+++ scripts/currentdate.js
@@ -0,0 +1,10 @@
+// Formatting for time display.
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+
+// Access DOM object for inserting the date.
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+// Create instance of date object.
+let todaysdate = new Date();
+
+// Create array for 
