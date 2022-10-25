47c50364ca15841fad0c3a0ef0aa588b6033d2fe
jadenmcc <jadenmccarrey@gmail.coms>
2022-10-25 11:14:39 -0600

    Continue troubleshooting JS for lazy loader.

--- lesson7/scripts/lazyLoader.js
+++ lesson7/scripts/lazyLoader.js
@@ -2,7 +2,7 @@ const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const options = {
     threshold: 0,
-    rootMargin: '0px 0px 0px 0px'
+    rootMargin: '0px 0px 500px 0px'
 };
 
 const loadImages = (image) => {
