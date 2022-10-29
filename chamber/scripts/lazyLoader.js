bffa4760dac9c980f1b64fe787c19c297436dfcc
jadenmcc <jadenmccarrey@gmail.coms>
2022-10-29 13:50:24 -0600

    Page weight downsizing.

--- chamber/scripts/lazyLoader.js
+++ chamber/scripts/lazyLoader.js
@@ -32,36 +32,38 @@ if ('IntersectionObserver' in window) {
     });
 };
 // //////////////////////////////////////////////////////////////////////////
-const imagesToLoad3 = document.querySelectorAll('source[data-srcset]');
 
-const options3 = {
-    // root: document.querySelector('main'),
-    threshold: 1.0,
-    rootMargin: '0px 0px 0px 0px'
-};
+// Muting below for the sake of page weight.
+// const imagesToLoad3 = document.querySelectorAll('source[data-srcset]');
 
-const loadImages3 = (image) => {
-    image.setAttribute('srcset', image.getAttribute('data-srcset'));
-    image.onload = () => {
-        image.removeAttribute('data-srcset');
-    };
-};
+// const options3 = {
+//     // root: document.querySelector('main'),
+//     threshold: 1.0,
+//     rootMargin: '0px 0px 0px 0px'
+// };
 
-if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages3(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    }, options3);
+// const loadImages3 = (image) => {
+//     image.setAttribute('srcset', image.getAttribute('data-srcset'));
+//     image.onload = () => {
+//         image.removeAttribute('data-srcset');
+//     };
+// };
 
-    imagesToLoad3.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    imagesToLoad3.forEach((img) => {
-        loadImages3(img);
-    });
-};
+// if ('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//         items.forEach((item) => {
+//             if (item.isIntersecting) {
+//                 loadImages3(item.target);
+//                 observer.unobserve(item.target);
+//             }
+//         });
+//     }, options3);
+
+//     imagesToLoad3.forEach((img) => {
+//         observer.observe(img);
+//     });
+// } else {
+//     imagesToLoad3.forEach((img) => {
+//         loadImages3(img);
+//     });
+// };
