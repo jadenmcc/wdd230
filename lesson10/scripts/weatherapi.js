1c847a3fcff2cf9780c1b1dd65f793162dd6bfb7
jadenmcc <jadenmccarrey@gmail.coms>
2022-11-16 18:12:01 -0700

    Added Weather API page for W10 assignment.

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,35 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// const url = 'http://api.openweathermap.org/geo/1.0/direct?q=Fairbanks,AK,USA&limit=1&units=imperial&appid=1157f36a52164b93b327dd19914f8b96'; This gives just geographical info.
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.837845&lon=-147.716675&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+};
