e6a11ef8d80b96047dec18b1c30c62052191586e
jadenmcc <jadenmccarrey@gmail.coms>
2022-11-18 11:11:58 -0700

    Finishing weather and windchill scripts.

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -1,17 +1,26 @@
 // select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const newCurrentTemp = document.querySelector('#currentTemp');
+const weatherIcon = document.querySelector('#weatherIcon');
+const captionDesc = document.getElementById('currentForecast');
+const windSpeed = document.getElementById('currentWind');
 
+// Farmington, UT
 // const url = 'http://api.openweathermap.org/geo/1.0/direct?q=Fairbanks,AK,USA&limit=1&units=imperial&appid=1157f36a52164b93b327dd19914f8b96'; This gives just geographical info.
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.837845&lon=-147.716675&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.9804423&lon=-111.8874688&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+// Cheyenne, WY
+// const url = 'http://api.openweathermap.org/geo/1.0/direct?q=Cheyenne,WY,USA&limit=1&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=41.139981&lon=-104.820246&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+// Fairbanks, AK
+// const url = 'http://api.openweathermap.org/geo/1.0/direct?q=Farmington,UT,USA&limit=1&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
+// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.837845&lon=-147.716675&units=imperial&appid=1157f36a52164b93b327dd19914f8b96';
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // this is for testing the call
+      // Below is for testing the call:
+      console.log(data);
       displayResults(data);
     } else {
         throw Error(await response.text());
@@ -19,12 +28,12 @@ async function apiFetch() {
   } catch (error) {
       console.log(error);
   }
-}
+};
 
 apiFetch();
 
 function displayResults(weatherData) {
-  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  newCurrentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
 
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
@@ -32,6 +41,8 @@ function displayResults(weatherData) {
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = titleCase(desc);
+
+  windSpeed.textContent = weatherData.wind.speed.toFixed(0);
 };
 
 function titleCase(str) {
